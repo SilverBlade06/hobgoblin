91498d2eeab228162337a2332d00df1d02a49d85
SilverBlade <feri.orovecz@gmail.com>
2016-05-19 22:35:48 +0200

    Added 3D vector, and some base code for controls(not working at all).

--- src/hobgoblin.cpp
+++ src/hobgoblin.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
 // OpenGL/Graphic libraries
 // Include GLEW (ALWAYS INCLUDE **BEFORE** GLFW)
@@ -16,6 +17,47 @@
 #include "player.h"
 #include "../system/diagnostics.h"
 #include "vertices.h"
+
+// 3D vector structure
+struct Vector {
+   float x, y, z;
+
+   Vector() {
+       x = 0;
+       y = 0;
+       z = 0;
+   }
+
+   Vector(float x, float y, float z = 0) {
+       this->x = x;
+       this->y = y;
+       this->z = z;
+   }
+
+   Vector operator*(float a) {
+       return Vector(x * a, y * a, z * a);
+   }
+
+   Vector operator+(const Vector& v) {
+       return Vector(x + v.x, y + v.y, z + v.z);
+   }
+
+   Vector operator-(const Vector& v) {
+       return Vector(x - v.x, y - v.y, z - v.z);
+   }
+
+   float operator*(const Vector& v) {
+       return (x * v.x + y * v.y + z * v.z);
+   }
+
+   Vector operator%(const Vector& v) {
+       return Vector(y*v.z-z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
+   }
+
+   float length() {
+       return sqrt(x * x + y * y + z * z);
+   }
+};
 
 GLuint generateVBO(GLuint vbo, GLsizeiptr size, const GLvoid * data) {
     glGenBuffers(1, &vbo);
@@ -227,6 +269,79 @@ int main() {
    // Cleanup
    SOIL_free_image_data(image2);
    glBindTexture(GL_TEXTURE_2D, 0);
+
+   //********************************************************************************************//
+   //   Controls, keyboard and mouse                                                             //
+   //********************************************************************************************//
+   float vector3D[3] = {0, 0, 5};
+   // horizontal angle : toward -Z
+   float horizontalAngle = 3.14f;
+   // vertical angle : 0, look at the horizon
+   float verticalAngle = 0.0f;
+   // Initial Field of View
+   float initialFoV = 45.0f;
+
+   float speed = 3.0f; // 3 units / second
+   float mouseSpeed = 0.005f;
+
+   // Get mouse position
+   double xpos, ypos;
+   glfwGetCursorPos(window, &xpos, &ypos);
+
+   // Reset mouse position for next frame
+   glfwSetCursorPos(window, 640/2, 640/2);
+
+   // Compute new orientation
+   horizontalAngle += mouseSpeed /** deltaTime */* float(1024/2 - xpos );
+   verticalAngle   += mouseSpeed /** deltaTime */* float( 768/2 - ypos );
+
+   // Direction : Spherical coordinates to Cartesian coordinates conversion
+   vector3D[0] = cos(verticalAngle) * sin(horizontalAngle);
+   vector3D[1] = sin(verticalAngle);
+   vector3D[2] = cos(verticalAngle) * cos(horizontalAngle);
+
+   // Right vector
+//   glm::vec3 right = glm::vec3(
+//       sin(horizontalAngle - 3.14f/2.0f),
+//       0,
+//       cos(horizontalAngle - 3.14f/2.0f)
+//   );
+//
+//   // Up vector : perpendicular to both direction and right
+//   glm::vec3 up = glm::cross( right, direction );
+//
+//   // Move forward
+//   if (glfwGetKey( GLFW_KEY_UP ) == GLFW_PRESS){
+//       position += direction * deltaTime * speed;
+//   }
+//   // Move backward
+//   if (glfwGetKey( GLFW_KEY_DOWN ) == GLFW_PRESS){
+//       position -= direction * deltaTime * speed;
+//   }
+//   // Strafe right
+//   if (glfwGetKey( GLFW_KEY_RIGHT ) == GLFW_PRESS){
+//       position += right * deltaTime * speed;
+//   }
+//   // Strafe left
+//   if (glfwGetKey( GLFW_KEY_LEFT ) == GLFW_PRESS){
+//       position -= right * deltaTime * speed;
+//   }
+//
+//   double currentTime = glfwGetTime();
+//   float deltaTime = float(currentTime - lastTime);
+//
+//   float FoV = initialFoV - 5 * glfwGetMouseWheel();
+//
+//   // Projection matrix : 45&deg; Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+//   ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
+//   // Camera matrix
+//   ViewMatrix       = glm::lookAt(
+//       position,           // Camera is here
+//       position+direction, // and looks here : at the same position, plus "direction"
+//       up                  // Head is up (set to 0,-1,0 to look upside-down)
+//   );
+
+
 
    //********************************************************************************************//
    //   Drawing loop                                                                             //
